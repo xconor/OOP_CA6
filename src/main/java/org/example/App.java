80041fb35ad6748ea04dc6ab0d92483777c7bfe5
xconor <d00261104@student.dkit.ie>
2023-05-05 01:37:41 +0100

    created menu

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,32 +1,67 @@
 package org.example;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws DaoException {
-        GameDAO gameDAO = new GameDAOImpl();
+        GameDAO IGameDao = new GameDAOImpl();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\nCall getAllGames()");
-        List<Game> games = gameDAO.getAllGames();
+        boolean quit = false;
+        int menuItem;
 
-        if (games.isEmpty()) {
-            System.out.println("There are no games");
-        } else {
-            for (Game game : games) {
-                System.out.println("Game: " + game);
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Get all games");
+            System.out.println("2. Get game by ID");
+            System.out.println("3. Quit");
+
+            System.out.print("Enter menu item: ");
+            menuItem = scanner.nextInt();
+
+            switch (menuItem) {
+                case 1:
+                    System.out.println("\nAll games:");
+                    List<Game> games = IGameDao.getAllGames();
+
+                    if (games.isEmpty())
+                        System.out.println("There are no games");
+                    else {
+                        for (Game game : games)
+                            System.out.println("Game: " + game.toString());
+                    }
+
+                    break;
+
+                case 2:
+                    System.out.print("\nEnter game ID: ");
+                    int id = scanner.nextInt();
+
+                    try {
+                        Game game = IGameDao.getGameById(id);
+
+                        if (game == null)
+                            System.out.println("Game with ID " + id + " not found");
+                        else
+                            System.out.println("\nGame with ID " + id + ": " + game.toString());
+                    } catch (DaoException e) {
+                        System.out.println(e.getMessage());
+                    }
+
+                    break;
+
+                case 3:
+                    quit = true;
+                    break;
+
+                default:
+                    System.out.println("Invalid menu item. Please try again.");
             }
-        }
+        } while (!quit);
 
-        System.out.println("\nCall getGameById(1)");
-        Game game = gameDAO.getGameById(1);
-
-        if (game == null) {
-            System.out.println("Game not found");
-        } else {
-            System.out.println("Game: " + game);
-        }
+        scanner.close();
     }
-
 }
 
 
